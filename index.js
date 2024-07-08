b7215bbde9471571fea7ca8c604f888b1d7a6804
anish acharya <109168743+Daddy-anish@users.noreply.github.com>
2024-07-08 15:42:53 +0545

    controller js
    commiting change

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
- 
+const productRoute = require("./routes/product.route.js");
+ const Product = require('./models/product.model.js')
+//middlewear
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+
+
+// routes
+app.use("/api/products", productRoute);
 
 
 
@@ -11,7 +20,14 @@ app.get("/" ,(req,res)=>{
 })
 
 
-
+app.post('/api/products', async(req,res) =>{
+             try {
+             const product =    await Product.create(req.body)
+             res.status(200).json(product)
+             } catch (error) {
+              res.status(500).json({message : error.message})
+             }
+})
 
 
 mongoose.connect("mongodb+srv://anishnira3:ywBaFWZHBJE3iTk9@cluster0.6rfq8fn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
